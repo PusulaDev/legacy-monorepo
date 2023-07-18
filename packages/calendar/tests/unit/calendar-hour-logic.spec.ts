63505151824ebfa89ef5ff1e67abdcee04d79b54
Rasim ÖNER <Rasim.oner@pusulayazilim.com.tr>
2023-07-18 20:42:53 +0300

    feat/shiftStartTime - add shiftStartTime props - create allMinutes with this prop.
    (https://pusulayazilimprojects.atlassian.net/browse/PT-262)[PT-262]

--- packages/calendar/tests/unit/calendar-hour-logic.spec.ts
+++ packages/calendar/tests/unit/calendar-hour-logic.spec.ts
@@ -9,10 +9,12 @@ describe("Calendar Hour Logic", () => {
     const minuteInterval = 25;
 
     const start = "08:00";
-    const end = "10:00"
-    const minutes = calendarHourLogic.createAllMinutes(start, end, minuteInterval);
+    const end = "10:00";
+    const shiftStart = "09:15";
+    const minutesDefault = calendarHourLogic.createAllMinutes(start, end, minuteInterval);
+    const minutesWithShiftStart = calendarHourLogic.createAllMinutes(start, end, minuteInterval, shiftStart);
 
-    const expected: MinuteInterval[] = [
+    const expectedDefault: MinuteInterval[] = [
       { from: "08:00", to: "08:25", text: "00", hour: { text: "08", value: 8 }, interval: 25 },
       { from: "08:25", to: "08:50", text: "25", hour: { text: "08", value: 8 }, interval: 25 },
       { from: "08:50", to: "09:15", text: "50", hour: { text: "08", value: 8 }, interval: 25 },
@@ -20,7 +22,14 @@ describe("Calendar Hour Logic", () => {
       { from: "09:40", to: "10:00", text: "40", hour: { text: "09", value: 9 }, interval: 20 },
     ];
 
-    expect(minutes).toEqual(expected);
+    const expectedWithShiftStart: MinuteInterval[] = [
+      { from: "08:00", to: "09:15", text: "00", hour: { text: "08", value: 8 }, interval: 75 },
+      { from: "09:15", to: "09:40", text: "15", hour: { text: "09", value: 9 }, interval: 25 },
+      { from: "09:40", to: "10:00", text: "40", hour: { text: "09", value: 9 }, interval: 20 },
+    ];
+
+    expect(minutesDefault).toEqual(expectedDefault);
+    expect(minutesWithShiftStart).toEqual(expectedWithShiftStart);
   });
 
 
